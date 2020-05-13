0a669c9bfd58ff026e42afe77f4bb2d6cdc4846e
cxw_mac <1179853281@qq.com>
2020-05-13 19:11:46 +0800

    z

---
+++ first.py
@@ -0,0 +1,13 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(400, 200)
+    w.move(300, 300)
+    w.setWindowTitle('First App')
+    w.show()
+
+    sys.exit(app.exec_())
