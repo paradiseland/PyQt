360b2f1cf0727a721df149e82ea2b72658339be9
cxw_mac <1179853281@qq.com>
2020-05-14 10:00:10 +0800

    tooltip

--- BasicDemo.py
+++ BasicDemo.py
@@ -1,8 +1,10 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
 
 from PyQt5.QtGui import QIcon
+
+from PyQt5.QtGui import QFont #for show text. tooltip
 
 class Demo(QWidget):
 
@@ -12,17 +14,30 @@ class Demo(QWidget):
         self.initUI()
     
     def initUI(self):
+        
 
-        # setGeometry == 
-        self.setGeometry(300, 300, 300, 220)
+
+        # setGeometry == resize&move
+        self.setGeometry(300, 300, 300, 220) # [[x, y, width, height.
         self.setWindowTitle('Icon')
+        """    ICON   """
         self.setWindowIcon(QIcon('box.png'))        
+        """  Button   """
+        button = QPushButton('Button', self)
+        """  TOOLTIP  """
+        self.setToolTip('This is a <b>Qwidiget</b> widget')
+        button.setToolTip('This is a <b>QpushButton</b> widget')
 
+        
+        
+        
         self.show()
-
+        
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     de = Demo()
+    app.setWindowIcon(QIcon('box.png'))  # here app.windowicon wil be  shown at dock in MacOs.
+    # 主循环
     sys.exit(app.exec_())
